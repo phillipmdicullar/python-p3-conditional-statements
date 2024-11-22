c58026038af803594f9872f16c8bfb2411bef86f
Philip Emdokolo <phillip.emdokolo@student.moringaschool.com>
2024-11-22 23:54:50 +0300

    completed control flows

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,48 @@
 
 def admin_login(username, password):
     # your code here
+    if username == 'admin' and password == '12345' or username == "ADMIN":
+        return "Access granted"
+    else:
+        return "Access denied"
     pass
+
+# "Access granted"
 
 def hows_the_weather(temperature):
     # your code here
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature < 40 or temperature < 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
     pass
 
-def fizzbuzz(num):
-    # your code here
-    pass
+def fizzbuzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return number
+
 
 def calculator(operation, num1, num2):
     # your code here
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        print("Invalid operation!")
+        return None
     pass
